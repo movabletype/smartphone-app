704f19dac68c98437e8506144cbb9121dde12fa6
CHEEBOW <cheebow@gmail.com>
2015-06-10 12:23:00 +0900

    ブロックの削除機能追加

--- MT_iOS/MT_iOS/Classes/ViewController/BlockEditorTableViewController.swift
+++ MT_iOS/MT_iOS/Classes/ViewController/BlockEditorTableViewController.swift
@@ -118,17 +118,14 @@ class BlockEditorTableViewController: BaseTableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            items.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.

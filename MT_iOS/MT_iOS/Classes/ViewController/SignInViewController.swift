e1abc785a6a0657b18b74349cb2d1fd038921498
CHEEBOW <cheebow@gmail.com>
2015-06-15 14:42:19 +0900

    ローディング画面のデザイン適用

--- MT_iOS/MT_iOS/Classes/ViewController/SignInViewController.swift
+++ MT_iOS/MT_iOS/Classes/ViewController/SignInViewController.swift
@@ -11,16 +11,24 @@ import UIKit
 class SignInViewController: BaseViewController {
 
     @IBOutlet weak var gearImage: UIImageView!
+    
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        gradientLayer.frame = self.view.bounds
+        let startColor = Color.loginBgStart.CGColor
+        let endColor = Color.loginBgEnd.CGColor
+        gradientLayer.colors = [startColor, endColor]
+        gradientLayer.locations = [0.0, 1.0]
+        self.view.layer.insertSublayer(gradientLayer, atIndex: 0)
         
         let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
         rotationAnimation.toValue = (M_PI / 180) * 360
         rotationAnimation.duration = 1.0
         rotationAnimation.repeatCount = Float.infinity
-        //self.gearImage.layer.addAnimation(rotationAnimation, forKey: "rotateAnimation")
+        self.gearImage.layer.addAnimation(rotationAnimation, forKey: "rotateAnimation")
     }
 
     override func didReceiveMemoryWarning() {

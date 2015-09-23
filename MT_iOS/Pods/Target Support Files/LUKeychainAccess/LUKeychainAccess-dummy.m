dca7793ebbd677b80adbe93553939dac45c4ac54
Yuji Takayama <taka.swordbreaker@gmail.com>
2015-09-23 20:11:20 +0900

    Update for iOS9

---
+++ MT_iOS/Pods/Target Support Files/LUKeychainAccess/LUKeychainAccess-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LUKeychainAccess : NSObject
+@end
+@implementation PodsDummy_LUKeychainAccess
+@end
---
+++ MT_iOS/Pods/Target Support Files/LUKeychainAccess/LUKeychainAccess-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "LUKeychainAccess.h"
+#import "LUKeychainErrorHandler.h"
+#import "LUKeychainServices.h"
+
+FOUNDATION_EXPORT double LUKeychainAccessVersionNumber;
+FOUNDATION_EXPORT const unsigned char LUKeychainAccessVersionString[];
+
